c437914be9476bd04ed7c055daaaaf14e0d7f97d
abhishek <abhishekd@nds.com>
2014-06-22 15:54:27 +0530

    Modified comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-#' Functions below allow creation of a matrix which can cache its inverse. 
-#' If the inverse is not cached or matrix changes, a fresh inverse is 
-#' computed and stored into the cache.
+## Functions below allow creation of a matrix which can cache its inverse. 
+## If the inverse is not cached or matrix changes, a fresh inverse is 
+## computed and stored into the cache.
 
-#' Creates a matrix which allows to cache its inverse.
-#' @param x input matrix
+## Creates a matrix which allows to cache its inverse.
+## @param mat input matrix
 makeCacheMatrix <- function(mat = matrix()) {
   invs <- NULL
   
@@ -29,11 +29,11 @@ makeCacheMatrix <- function(mat = matrix()) {
 }
 
 
-#' cacheSolve - If the matrix object does not have a inverse Computed 
-#' and cached the inverse is computed, cached into the object and 
-#' returned as a return value.
-#' 
-#' @param x cacheable matrix
+## cacheSolve - If the matrix object does not have a inverse Computed 
+## and cached the inverse is computed, cached into the object and 
+## returned as a return value.
+## 
+## @param mat cacheable matrix, capable of storing the inverse.
 
 cacheSolve <- function(mat, ...) {
   ## Return a matrix that is the inverse of 'x'
