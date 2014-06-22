e06d751705b069667d66cf0151533d427fe5e023
abhishek <abhishekd@nds.com>
2014-06-22 15:52:47 +0530

    added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,54 @@
-## Functions below allow creation of a matrix which can cache its inverse. 
-## If the inverse is not cached or matrix changes, a fresh inverse is 
-## computed and stored into the cache.
+#' Functions below allow creation of a matrix which can cache its inverse. 
+#' If the inverse is not cached or matrix changes, a fresh inverse is 
+#' computed and stored into the cache.
 
-## makeCacheMatrix - Creates a matrix which allows to cache its inverse.
-
-makeCacheMatrix <- function(x = matrix()) {
+#' Creates a matrix which allows to cache its inverse.
+#' @param x input matrix
+makeCacheMatrix <- function(mat = matrix()) {
   invs <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
   
-  get <- function() x
+  ## function to update the stored matrix to new value
+  set <- function(newmat) {
+    mat <<- newmat
+    ## reset inverse if matrix changes.
+    invs <<- NULL
+  }
+
+  ## function to return the stored matrix
+  get <- function() mat
+  
+  ## function to set the inverse of the matrix for caching.
   setinv <- function(inverse) invs <<- inverse
+  
+  ## function to return the cached value of matrix, NULL if not set
   getinv <- function() invs
+
   list(set=set, get=get, 
        setinv=setinv, 
        getinv=getinv)
 }
 
 
-## cacheSolve - If the matrix object does not have a inverse Computed 
-## and cached the inverse is computed, cached into the object and 
-## returned as a return value.
+#' cacheSolve - If the matrix object does not have a inverse Computed 
+#' and cached the inverse is computed, cached into the object and 
+#' returned as a return value.
+#' 
+#' @param x cacheable matrix
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(mat, ...) {
   ## Return a matrix that is the inverse of 'x'
-  invs <- x$getinv()
+  invs <- mat$getinv()
+  ## if a cached inverse exists return it.
   if(!is.null(invs)) {
     message("getting cached data")
     return(invs)
   }
-  data <- x$get()
+  
+  ## get the matrix data anc compute the inverse.
+  data <- mat$get()
   invs <- solve(data, ...)
-  x$setinv(invs)
+  
+  ## cache the newly computed inverse in the matrix object.
+  mat$setinv(invs)
   invs
 }
